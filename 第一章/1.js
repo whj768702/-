163460c86687a543b160c0a004175099071c5feb
whj768702 <whj768702@gmail.com>
2021-01-12 21:32:56 +0800

    refactor(第一章): 内联变量  提取函数。

--- 第一章/1.js
+++ 第一章/1.js
@@ -2,19 +2,25 @@ const plays = require('./plays.js');
 const invoice = require('./invoice.js');
 
 function statement(invoice) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
+  let totalAmount = applySauce();
   let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+}
+function applySauce() {
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+  return totalAmount;
 }
 function totalVolumeCredits() {
   let volumeCredits = 0;
