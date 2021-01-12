51b9b70929ebc2d3a55a415c50c07f3b966a427e
whj768702 <whj768702@gmail.com>
2021-01-12 21:28:30 +0800

    refactor(第一章): 提炼函数(106) 内联变量(123)。
    提炼totalVolumeCredits函数，然后内联该函数。

--- 第一章/1.js
+++ 第一章/1.js
@@ -10,13 +10,18 @@ function statement(invoice) {
     totalAmount += amountFor(perf);
   }
 
+  let volumeCredits = totalVolumeCredits();
+
+  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+function totalVolumeCredits() {
   let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+  return volumeCredits;
 }
 
 function usd(aNumber) {
