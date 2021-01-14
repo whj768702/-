7c516a4af82b024ba098e33251408f0bd89f66bd
whj768702 <whj768702@gmail.com>
2021-01-14 22:08:04 +0800

    refactor(第一章): 提取函数。
    提取构造数据格式的逻辑到函数。

--- 第一章/1.js
+++ 第一章/1.js
@@ -2,44 +2,73 @@ const plays = require('./plays.js');
 const invoice = require('./invoice.js');
 
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {
     customer: invoice.customer,
     performances: invoice.performances.map(enrichPerformance),
   };
-  return renderPlainText(statementData, plays);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return statementData;
+}
+function enrichPerformance(aPerformance) {
+  const result = Object.assign({}, aPerformance);
+  result.play = playFor(aPerformance);
+  result.amount = amountFor(result);
+  result.volumeCredits = volumeCreditsFor(result);
+  return result;
+}
 
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(aPerformance);
-    result.amount = amountFor(result);
-    return result;
-  }
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
+function amountFor(aPerformance) {
+  let result = 0;
+  switch (aPerformance.play.type) {
+    case 'tragedy':
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${aPerformance.play.type}`);
   }
+  return result;
+}
 
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
+function volumeCreditsFor(aPerformance) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(aPerformance.audience - 30, 0);
+  // add extra credit for every ten comedy attendees
+  if ('comedy' === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
+  return volumeCredits;
+}
+
+function totalAmount(data) {
+  let result = 0;
+  for (let perf of data.performances) {
+    result += perf.amount;
   }
+  return result;
+}
+function totalVolumeCredits(data) {
+  let volumeCredits = 0;
+  for (let perf of data.performances) {
+    volumeCredits += perf.volumeCredits;
+  }
+  return volumeCredits;
 }
 
 function renderPlainText(data, plays) {
@@ -49,25 +78,9 @@ function renderPlainText(data, plays) {
     result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
@@ -75,14 +88,6 @@ function renderPlainText(data, plays) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
-    return volumeCredits;
   }
 }
 
