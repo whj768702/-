491ae60da6f34f2f1e66c81c4a76f05263e0d490
whj768702 <whj768702@gmail.com>
2021-01-15 21:41:06 +0800

    refactor(第一章): 以多态取代条件表达式(272)
    创建一个类，将价格计算函数和观众量积分计算函数放进去。

--- 第一章/createStatementData.mjs
+++ 第一章/createStatementData.mjs
@@ -1,3 +1,31 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performances = aPerformance;
+    this.play = aPlay;
+  }
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (this.performances.audience > 30) {
+          result += 1000 * (this.performances.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (this.performances.audience > 20) {
+          result += 10000 + 500 * (this.performances.audience - 20);
+        }
+        result += 300 * this.performances.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
+}
+
 function createStatementData(invoice, plays) {
   const result = {
     customer: invoice.customer,
@@ -8,8 +36,9 @@ function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(aPerformance);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -20,25 +49,7 @@ function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
