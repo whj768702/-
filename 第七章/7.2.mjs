e761ec00e880a0f0a74ce7a57afeb970376abfee
whj768702 <whj768702@gmail.com>
2021-01-17 19:56:32 +0800

    refactor(2): 在类上添加添加和移除函数。

--- 第七章/7.2.mjs
+++ 第七章/7.2.mjs
@@ -12,6 +12,19 @@ class Person {
   set course(aList) {
     this._courses = aList;
   }
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+  removeCourse(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    },
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
+  }
 }
 class Course {
   constructor(name, isAdvanced) {
